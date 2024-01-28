2d89f8d0b1d60f230ab91fcb9d110c6242544321
Emily Liang <exliang@uci.edu>
2024-01-28 13:00:56 -0800

    passed autograder

--- a1.py
+++ a1.py
@@ -7,8 +7,6 @@
 
 from pathlib import Path, PurePath
 
-#issues: path is invalid bc there are \\ rather than \ & autograder
-
 
 def main():
     user_input()
@@ -16,97 +14,84 @@ def main():
 
 def user_input():
     while True:
-        user_command = input()  # format: [COMMAND] [INPUT] [[-]OPTION] [INPUT] 
+        user_command = input()  # format: [COMMAND] [INPUT] [[-]OPTION] [INPUT]
         command_list = user_command.split()
         command = command_list[0]
 
         if command == 'Q':
             quit()
-
-        # ensuring proper whitespace handling
-        path = [command_list[1]]
-        space_count = 0
-        if not command_list[2].startswith("-"):  # path had whitespace in it
-        	for i in range(2, len(command_list)):
-        		if not command_list[i].startswith("-"):
-        			path.append(command_list[i])
-        			space_count += 1
-        	newpath = " ".join(path)
-        	command_list.insert(1, newpath)  # insert new path in command_list
-        	# get rid of old path in command_list (num spaces = (num spaces + 1) items in list)
-        	for i in range(space_count, -1, -1):
-        		print("command_list[i+2]",command_list[i+2])
-        		command_list.pop(i+2)
-
-        path = command_list[1]
-        myPath = Path(path)
-
-        if myPath.exists():  # ensure that directory exists
-            if command == 'L':  # list contents of directory
-                if len(command_list) == 2:  # [COMMAND] [INPUT]
-                    list_directories(myPath)
-                elif len(command_list) == 3:  # [COMMAND] [INPUT] [[-]OPTION]
-                    option = command_list[2]
-                    if option == '-r':
-                        recursive(myPath)
-                    elif option == '-f':  # output files only
-                        list_files(myPath)
-                    else:  # invalid command
-                    	print("ERROR")
-                elif len(command_list) == 4:  # [C][I][[-]O][I]
-                    option = command_list[2]
-                    if option == '-s':  # output files that match the file name
-                        file_name = command_list[3]
-                        if "." not in file_name:  # ensure a file is entered
-                        	print("ERROR")
-                        else:
-                        	matching_files(myPath, file_name)
-                    elif option == '-e':
-                        file_extension = command_list[3]
-                        if len(file_extension) != 3:
-                        	print("ERROR")
-                        else:
-                        	matching_extension(myPath, file_extension)
-                    elif option == '-r':  # -r -f
-                        option2 = command_list[3]
-                        recursive_f(myPath)
-                    else:  # invalid command
-                    	print("ERROR")
-                elif len(command_list) == 5:  # [C][I][[-]O][I][I]
-                    option = command_list[2]
-                    option2 = command_list[3]
-                    if option == '-r' and option2 == '-s':  # -r -s filename.ex
-                        file_name = command_list[4]
-                        if "." not in file_name:  # ensure a file is entered
-                        	print("ERROR")
-                        else:
-                        	recursive_s(myPath, file_name)
-                    elif option == '-r' and option2 == '-e':  # -r -e fileex
-                        file_extension = command_list[4]
-                        if file_extension.isnumeric():  #if fileex contains nums
-                        	print("ERROR")
-                        else:
-                        	recursive_e(myPath, file_extension)
-                    else:  # invalid command
-                    	print("ERROR")
-            elif command == "C":  # create new DSU file
-            	filename = command_list[3]
-            	if command_list[2] != "-n":
-            		print("ERROR")
-            	else:
-            		command_C(myPath, filename)
-            elif command == "D":  # delete DSU file
-            	command_D(myPath)
-            elif command == "R":  # read file contents
-            	command_R(myPath)
-            else:  # invalid command
-            	print("ERROR")
-            	get_correct_file()
+        elif len(command_list) == 1:  # user only inputs a letter & no dir
+            print("ERROR")
         else:
-            if command == "D" or command == "R":
-                command_D(myPath)
+            path = command_list[1]
+            myPath = Path(path)
+
+            if myPath.exists():  # ensure that directory exists
+                if command == 'L':  # list contents of directory
+                    if len(command_list) == 2:  # [COMMAND] [INPUT]
+                        list_directories(myPath)
+                    elif len(command_list) == 3:  # [C] [INPUT] [[-]OPTION]
+                        option = command_list[2]
+                        if option == '-r':
+                            recursive(myPath)
+                        elif option == '-f':  # output files only
+                            list_files(myPath)
+                        else:  # invalid command
+                            print("ERROR")
+                    elif len(command_list) == 4:  # [C][I][[-]O][I]
+                        option = command_list[2]
+                        if option == '-s':  # output files that match file name
+                            file_name = command_list[3]
+                            if "." not in file_name:  # ensure file is entered
+                                print("ERROR")
+                            else:
+                                matching_files(myPath, file_name)
+                        elif option == '-e':
+                            file_extension = command_list[3]
+                            if len(file_extension) != 3:
+                                print("ERROR")
+                            else:
+                                matching_extension(myPath, file_extension)
+                        elif option == '-r':  # -r -f
+                            option2 = command_list[3]
+                            recursive_f(myPath)
+                        else:  # invalid command
+                            print("ERROR")
+                    elif len(command_list) == 5:  # [C][I][[-]O][I][I]
+                        option = command_list[2]
+                        option2 = command_list[3]
+                        if option == '-r' and option2 == '-s':  # -r -s filen.e
+                            file_name = command_list[4]
+                            if "." not in file_name:  # ensure file is entered
+                                print("ERROR")
+                            else:
+                                recursive_s(myPath, file_name)
+                        elif option == '-r' and option2 == '-e':  # -r -e filee
+                            file_extension = command_list[4]
+                            if file_extension.isnumeric():  # fileex has nums
+                                print("ERROR")
+                            else:
+                                recursive_e(myPath, file_extension)
+                        else:  # invalid command
+                            print("ERROR")
+                elif command == "C":  # create new DSU file
+                    filename = command_list[3]
+                    if command_list[2] != "-n":
+                        print("ERROR")
+                    else:
+                        command_C(myPath, filename)
+                elif command == "D":  # delete DSU file
+                    command_D(myPath)
+                elif command == "R":  # read file contents
+                    command_R(myPath)
+                else:  # invalid command
+                    print("ERROR")
+                    get_correct_file()
             else:
-            	print("Directory doesn't exist. Try again.")
+                if command == "D" or command == "R":
+                    command_D(myPath)
+                else:
+                    print("Directory doesn't exist. Try again.")
 
 
 def list_directories(myPath):
@@ -196,50 +181,43 @@ def command_C(myPath, filename):
 
 
 def command_D(myPath):
-	while True:
-		dsufile = get_path_parts(myPath)
-		if not dsufile.endswith(".dsu"):  # if file isn't DSU file
-			print("ERROR")
-			myPath = get_correct_file(dsufile)  # so that myPath changes 
-		else:  # file is DSU file 
-			Path.unlink(dsufile)  # delete file from path
-			print(myPath, "DELETED")  # output the path
-			break
+    while True:
+        dsufile = get_path_parts(myPath)
+        if not dsufile.endswith(".dsu"):  # if file isn't DSU file
+            print("ERROR")
+            myPath = get_correct_file(dsufile)  # so that myPath changes
+        else:  # file is DSU file
+            Path.unlink(dsufile)  # delete file from path
+            print(myPath, "DELETED")  # output the path
+            break
 
 
 def command_R(myPath):
-	while True:
-		dsufile = get_path_parts(myPath)
-		if not dsufile.endswith(".dsu"):  # if file isn't DSU file
-			print("ERROR")
-			myPath = get_correct_file(dsufile)
-		elif myPath.stat().st_size == 0:  # file_size = myPath.stat().st_size
-			print("EMPTY")
-			myPath = get_correct_file(dsufile)
-		else:  # print file contents
-			with open(dsufile, "r") as myFile:
-				for line in myFile:
-					line = line.strip()  # get rid of whitespace
-					print(line)
-			break
+    while True:
+        dsufile = get_path_parts(myPath)
+        if not dsufile.endswith(".dsu"):  # if file isn't DSU file
+            print("ERROR")
+            myPath = get_path()
+        elif myPath.stat().st_size == 0:  # file_size = myPath.stat().st_size
+            print("EMPTY")
+            myPath = get_path()
+        else:  # print file contents
+            print(myPath.read_text().strip())
+            break
 
 
-def get_correct_file(dsufile):
-	user_command = input()  # keep on asking for input
-	command_list = user_command.split()
-	myPath = Path(command_list[1])
-	return myPath
+def get_path():
+    user_command = input()  # keep on asking for input
+    command_list = user_command.split()
+    myPath = Path(command_list[1])
+    return myPath
 
 
 def get_path_parts(myPath):
-	p = PurePath(myPath)
-	dir_tuple = p.parts[1:]  # getting parts of dir (ignoring C:\)
-	dsufile = dir_tuple[len(dir_tuple)-1]
-	return dsufile
-
-
-def whitespace_checker():  # handling whitespace (if a file has a space in name)
-	pass
+    p = PurePath(myPath)
+    dir_tuple = p.parts[1:]  # getting parts of dir (ignoring C:\)
+    dsufile = dir_tuple[len(dir_tuple)-1]
+    return dsufile
 
 
 if __name__ == '__main__':
@@ -247,3 +225,18 @@ if __name__ == '__main__':
 
 # Citations:
 # - https://docs.python.org/3/library/pathlib.html
+
+# # ensuring proper whitespace handling
+# path = [command_list[1]]
+# space_count = 0
+# if not command_list[2].startswith("-"):  # path had whitespace in it
+# 	for i in range(2, len(command_list)):
+# 		if not command_list[i].startswith("-"):
+# 			path.append(command_list[i])
+# 			space_count += 1
+# 	newpath = " ".join(path)
+# 	command_list.insert(1, newpath)  # insert new path in command_list
+# 	# get rid of old path in command_list (num spaces = (num spaces + 1) items in list)
+# 	for i in range(space_count, -1, -1):
+# 		print("command_list[i+2]",command_list[i+2])
+# 		command_list.pop(i+2)
